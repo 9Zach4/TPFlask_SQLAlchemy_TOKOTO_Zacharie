9b5c13087ccd7e829b2656a461a07bf3df562c05
Zach <djessitokoto@gmail.com>
2024-02-10 02:47:26 +0100

    migration ok

--- src/critique_film/models.py
+++ src/critique_film/models.py
@@ -2,23 +2,23 @@
 from .database import db
 from datetime import datetime
 
-class Clients(db.Model):
+class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nom = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
-    test = db.Column(db.String(80), unique=True, nullable=False)
-
-
+  
 class Chambre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nurméro = db.Column(db.String(10), unique=True, nullable=False)
+    numero = db.Column(db.String(10), unique=True, nullable=False)  
+    type = db.Column(db.String(50), nullable=False)
     prix = db.Column(db.Float, nullable=False)
     reservations = db.relationship('Reservation', backref='chambre', lazy='dynamic')
 
 
 class Reservation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    id_chambre = db.Column(db.Integer, db.ForeignKey('chambre.id'))
+    id_client = db.Column(db.Integer, db.ForeignKey('client.id'))  
+    id_chambre = db.Column(db.Integer, db.ForeignKey('chambre.id'))  
     date_arrivee = db.Column(db.DateTime, nullable=False)
     date_depart = db.Column(db.DateTime, nullable=False)
     statut = db.Column(db.String(50), nullable=False)
