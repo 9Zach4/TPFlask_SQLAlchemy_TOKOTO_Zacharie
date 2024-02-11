ff547ecb45c6cf3cb55ac6feb543daa45e7dd2a0
Zach <djessitokoto@gmail.com>
2024-02-11 14:04:57 +0100

    requête get et post fonctionnel reservation

--- src/hotel/routes.py
+++ src/hotel/routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, jsonify, request
 from .database import db
-from .models import Client, Chambre
+from .models import Client, Chambre, Reservation
+from datetime import datetime
 
 main = Blueprint('main', __name__)
 
@@ -10,7 +11,7 @@ def index():
     return render_template('index.html')
 
 
-@main.route('/api/inscription')
+
 def inscription():
     new_clients = [
         Client(name='Moussa', email='moussa@gmail.com'),
@@ -21,9 +22,23 @@ def inscription():
     db.session.commit()
     return jsonify(Client.name)
 
+@main.route('/api/client/liste', methods=['GET'])
+def client_list():
+    clients = Client.query.all()
+    client_liste = []
+    for client in clients:
+        client_dict = {
+            'id': client.id,
+            'name': client.name,
+            'email': client.email,
+          
+        }
+        client_liste.append(client_dict)
+    return jsonify(client_liste), 200
 
-@main.route('/api/chambre-liste')
-def chambre_liste():
+
+
+def room_list():
   simpleRoom = [
     Chambre(number=1, type='simple', price=80),
     Chambre(number=2, type='simple', price=80),
@@ -46,3 +61,84 @@ def chambre_liste():
   db.session.add_all(simpleRoom)
   db.session.add_all(luxuryRoom)
   db.session.commit()
+
+@main.route('/api/chambres/liste', methods=['GET'])
+def room_list():
+
+    chambres = Chambre.query.all()
+
+    chambre_liste = []
+    for chambre in chambres:
+        chambre_dict = {
+            'numero': chambre.number,
+            'type': chambre.type,
+            "id": chambre.id,
+            'prix': chambre.price
+        }
+        chambre_liste.append(chambre_dict)
+
+    return jsonify(chambre_liste), 200
+
+@main.route('/api/chambres/disponibles', methods=['GET'])
+def room_available():
+    date_arrivee_str = request.args.get('date_arrivee')
+    date_depart_str = request.args.get('date_depart')
+
+    date_arrivee = datetime.strptime(date_arrivee_str, '%Y-%m-%d')
+    date_depart = datetime.strptime(date_depart_str, '%Y-%m-%d')
+
+    chambres = Chambre.query.all()
+    chambres_disponibles = []
+    for chambre in chambres:
+        reservations = Reservation.query.filter(Reservation.id_chambre == chambre.id).all()
+        chambre_dict = {
+            'numero': chambre.number,
+            'type': chambre.type,
+            'prix': chambre.price
+        }
+        chambre_disponible = True
+        for reservation in reservations:
+            if date_arrivee < reservation.departure_date and date_depart > reservation.arrival_date:
+                chambre_disponible = False
+        if chambre_disponible:
+            chambres_disponibles.append(chambre_dict)
+    return jsonify(chambres_disponibles), 200
+
+@main.route('/api/reservations', methods=['POST'])
+def reservation():
+    id_client = request.json.get('id_client')
+    id_chambre = request.json.get('id_chambre')
+    date_arrivee = request.json.get('date_arrivee')
+    date_depart = request.json.get('date_depart')
+
+    chambre_disponible = check_room_availability(id_chambre, date_arrivee, date_depart)
+
+    
+    if not chambre_disponible:
+        return jsonify({"success": False, "message": "La chambre n'est pas disponible pour les dates demandées."}), 400
+
+
+    reservation = Reservation(
+        id_client=id_client,
+        id_chambre=id_chambre,
+        arrival_date=date_arrivee,
+        departure_date=date_depart,
+        statut='en attente'
+    )
+    db.session.add(reservation)
+    db.session.commit()
+    return jsonify('Reservation effectuee'), 201
+
+def check_room_availability(id_chambre, date_arrivee, date_depart):
+  
+    date_arrivee = datetime.strptime(date_arrivee, '%Y-%m-%d')
+    date_depart = datetime.strptime(date_depart, '%Y-%m-%d')
+
+    
+    reservations = Reservation.query.filter_by(id_chambre=id_chambre).all()
+
+    for reservation in reservations:
+        if date_arrivee < reservation.departure_date and date_depart > reservation.arrival_date:
+            return False
+
+    return True
